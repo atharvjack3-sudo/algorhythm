3b75d3165a8403312c7c577f9bbb9871ba4aa2fd
atharvjack3-sudo <atharvjack3@gmail.com>
2026-01-22 21:25:55 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,13 @@
 import "../env.js";
 import mysql from "mysql2/promise";
 
+console.log("DB CONFIG CHECK:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  db: process.env.DB_NAME,
+});
+
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
