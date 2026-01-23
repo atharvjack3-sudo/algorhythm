c3938fe8c6237c0042b232ffbb05be1325333c1d
atharvjack3-sudo <atharvjack3@gmail.com>
2026-01-23 14:10:11 +0530

    added port config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -11,6 +11,7 @@ console.log("DB CONFIG CHECK:", {
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: Number(process.env.DB_PORT),
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 
