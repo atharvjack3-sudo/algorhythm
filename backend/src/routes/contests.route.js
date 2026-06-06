152be81b78a18054566d8fcb4e00d98f329be045
atharvjack3-sudo <atharvjack3@gmail.com>
2026-06-06 21:16:17 +0530

    Update contests.route.js
    updated headers for RapidAPI

--- backend/src/routes/contests.route.js
+++ backend/src/routes/contests.route.js
@@ -975,7 +975,13 @@ router.post(
             expected_output: Buffer.from(expectedOutput).toString("base64"),
             cpu_time_limit: timeLimit,
             memory_limit: memoryLimit
+          },
+        {
+          headers: {
+            "X-RapidAPI-Key": process.env.JUDGE0_API_KEY,
+            "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
           }
+        }
         );
 
         // Parse metrics
